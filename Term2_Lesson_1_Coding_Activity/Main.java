24de3d8958ac4c881e4ade8512a72daf6ed8f2e3
sm52 <48102667+sm52@users.noreply.github.com>
2020-01-06 15:23:33 -0800

    Add files via upload

---
+++ Term2_Lesson_1_Coding_Activity/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner freezer = new Scanner(System.in);
+        int count = 0;
+        boolean finish = false;
+        String b;
+        ArrayList <String> list = new ArrayList();
+        System.out.println("Please enter words, enter STOP to stop the loop.");
+        while(finish == false){
+            b = freezer.nextLine();
+            count ++;
+            list.add(b);
+            if(b.equals("STOP")){
+                count --;
+                finish = true;
+                break;
+            }
+        }
+        list.remove("STOP");
+        System.out.println(list.size());
+        System.out.println(list);
+        list.remove(0);
+        list.remove(count-2);
+        System.out.println(list);
+    }
+}
